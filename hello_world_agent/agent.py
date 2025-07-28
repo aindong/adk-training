105ed36054b815e48916d5d7f9ea78888a9bf56f
Alleo Indong <alleo.indong@gmail.com>
2025-07-28 17:40:49 +0800

    Add initial implementation of hello_world_agent with date functionality and .gitignore file
    - Created .gitignore to exclude environment and Python bytecode files.
    - Added __init__.py to the hello_world_agent package.
    - Implemented agent.py with a simple agent that greets the user and provides the current date.

---
+++ hello_world_agent/agent.py
@@ -0,0 +1,18 @@
+import datetime
+from google.adk.agents import Agent
+
+def get_current_date():
+    return datetime.datetime.now().strftime("%Y-%m-%d")
+
+root_agent = Agent(
+    name="hello_world_agent",
+    model='gemini-2.5-flash-lite',
+    description="The hello world agent is a simple agent that says hello world and mentions the current day",
+    tools=[get_current_date],
+    instruction="""
+    You are a helpful assistant that greets the user and mentions the current day and give some random trivia.
+    Like this:
+    Hello, today is Monday, 28th July 2025.
+    You can use the tools to get the current date.
+    """,
+)
